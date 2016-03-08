f3244dc8295b09808dd21e2813897411ec229747
Frank Edward Daza González <frank.daza2@gmail.com>
2016-03-07 21:17:20 -0500

    Update Suma.java

--- Suma/src/co/edu/usbcali/suma/Suma.java
+++ Suma/src/co/edu/usbcali/suma/Suma.java
@@ -3,9 +3,10 @@ package co.edu.usbcali.suma;
 public class Suma {
 
 	public static void main(String[] args) {
-		Integer a = 1234;
-		Integer b = 23;
+		Integer a = 666;
+		Integer b = 999;
 		
+		System.out.println("Yo quiero mas amor!!!!");
 		System.out.println("La suma es: " + (a + b));
 	}
 
