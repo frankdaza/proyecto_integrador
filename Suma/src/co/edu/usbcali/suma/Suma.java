6ea3fa55ce5743e6964cb17632bb9b4f87b849aa
Frank Edward Daza González <frank.daza2@gmail.com>
2016-03-07 21:31:43 -0500

    Arreglados bugs!

--- Suma/src/co/edu/usbcali/suma/Suma.java
+++ Suma/src/co/edu/usbcali/suma/Suma.java
@@ -2,17 +2,14 @@ package co.edu.usbcali.suma;
 
 public class Suma {
 
-	public static void main(String[] args) {
-		System.out.println("Me gusta the walking dead!!!!");
+	public static void main(String[] args) {		
 		System.out.println("Hello World!");
-		
-		
-		Integer a = 666;
+				
 		Integer b = 23;
 		Integer c = 123;
 		
 		System.out.println("Yo quiero mas amor!!!!");
-		System.out.println("La suma es: " + (a + b + c));
+		System.out.println("La suma es: " + (b + c));
 	}
 
 }
