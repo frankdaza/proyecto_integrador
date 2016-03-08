03c672e6653c1556ce49478c67cbd9c0209b7702
Frank Edward Daza González <frank.daza2@gmail.com>
2016-03-07 21:16:05 -0500

    Se creo la clase suma, la cual suma dos números.

---
+++ Suma/src/co/edu/usbcali/suma/Suma.java
@@ -0,0 +1,12 @@
+package co.edu.usbcali.suma;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		Integer a = 1234;
+		Integer b = 23;
+		
+		System.out.println("La suma es: " + (a + b));
+	}
+
+}
